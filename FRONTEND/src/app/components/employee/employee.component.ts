ef92e1c0c28babe4c7ea4fd34f0243462e290ea4
Wilson Castro <wilsoncastro0710@gmail.com>
2021-04-22 22:35:15 -0500

    Cabmiso

--- FRONTEND/src/app/components/employee/employee.component.ts
+++ FRONTEND/src/app/components/employee/employee.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+//Importando servicio
+import { EmployeeService } from '../../services/employee.service';
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.scss']
+  styleUrls: ['./employee.component.scss'],
 })
 export class EmployeeComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private employeService: EmployeeService) {}
+  //Cuando carga el componente
   ngOnInit(): void {
+    this.employeService.getEmployees();
+    console.log('Cargando useeffect');
   }
-
 }
